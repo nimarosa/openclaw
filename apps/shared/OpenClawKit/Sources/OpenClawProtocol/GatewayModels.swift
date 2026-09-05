ed8dd5aad8e9ec9e96e4e857e04cf6a72815a0e9
Patrick Erichsen <patrick.a.erichsen@gmail.com>
2026-09-05 07:59:42 -0700

    build(protocol): refresh plugin detail models

--- apps/shared/OpenClawKit/Sources/OpenClawProtocol/GatewayModels.swift
+++ apps/shared/OpenClawKit/Sources/OpenClawProtocol/GatewayModels.swift
@@ -22536,11 +22536,14 @@ public struct PluginsCatalogGetParams: Codable, Sendable {
 
 public struct PluginsCatalogGetResult: Codable, Sendable {
     public let plugin: PluginDiscoveryEntry
+    public let detail: [String: AnyCodable]
 
     public init(
-        plugin: PluginDiscoveryEntry)
+        plugin: PluginDiscoveryEntry,
+        detail: [String: AnyCodable])
     {
         self.plugin = plugin
+        self.detail = detail
     }
 }
 
